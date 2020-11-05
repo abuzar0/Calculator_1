8117e69e26cda9f6a242ddd074e600e094882222
abuzar0 <abuzarchaudhary999@gmail.com>
2020-11-05 13:29:36 -0800

    First Commit

---
+++ includes/script.js
@@ -0,0 +1,15 @@
+function call() {
+        var result = document.getElementById('result');
+}
+function call2() {
+        result.value = eval(result.value);
+}
+addEventListener('click', (e) => {
+        screenvalue = e.target.innerText;
+        if (screenvalue == 'DEL') {
+                console.log(result.value.toString());
+        }
+        else {
+                result.value += screenvalue;
+        }
+})
